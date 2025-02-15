e7093f6cfcca19373ed8c1555ef44c6579e46e01
atarwn <a@qwa.su>
2025-02-16 01:53:42 +0500

    no nogroup only nobodu

--- slock-1.5/config.h
+++ slock-1.5/config.h
@@ -1,10 +1,10 @@
 /* user and group to drop privileges to */
 static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "#1f1a1b",     /* after initialization */
-	[INPUT] =  "#ffb0cb",   /* during input */
+	[INPUT] =  "#2f2a2b",   /* during input */
 	[FAILED] = "#d63e3e",   /* wrong password */
 };
 
